4a6fd153182338be6ee8082dad74c30e5e62cb9a
Conor Smyth <cnrsmyth@gmail.com>
2016-04-30 23:01:07 +0100

    Added hybrid directory

---
+++ hybrid_examples/make.sh
@@ -0,0 +1,18 @@
+TEST="$(echo $1 | grep \\.)"
+
+if [ -z $1 ]; then
+	echo -e "usage ./make.sh filename"
+	exit;
+fi
+
+if [ $TEST ]; then
+	echo -e "Leave out .c/.cpp"
+	exit;
+fi
+
+TEST=$1.cpp
+if [ -f $TEST ]; then
+	mpicxx -fopenmp $1.cpp -o $1.o
+else
+	mpicc -fopenmp $1.c -o  $1.o
+fi
